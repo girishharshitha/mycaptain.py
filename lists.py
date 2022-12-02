c6471ffaf8327820230df304dd7bfdbf473330dc
girishharshitha <118171462+girishharshitha@users.noreply.github.com>
2022-12-02 13:56:22 +0530

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,10 @@
+list1 = [12,-7,5,64,-14]
+print("\n")
+print("List1 = ")
+print([a for j,a in enumerate(list1) if a>=0])
+
+
+list2 = [12,14,-95,3]
+print("\n")
+print("List2 = ")
+print([a for j,a in enumerate(list2) if a>=0])
